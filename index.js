ccc8cc50b46c53453a7c9a7d66a4921f29738b10
Omar Brisita <o.brisita@gmail.com>
2021-11-30 20:15:09 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Susie", "Omar J", "Micah"]
+
+function writeCards(names, event) {
+    const message = []
+     for (let i = 0; i < names.length; i++) {
+         message[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+         debugger;
+     }
+     return message;
+}
+writeCards(names, "birthday");
+
+function countDown(num) {
+    while (num>= 0){
+        console.log(num)
+        num--
+    }
+}
